b023f2eb347da6354d88ca2449c5317505ebdf82
Sylvain Afchain <safchain@gmail.com>
2015-04-15 00:13:44 +0200

    Add the arduino target and a first example

---
+++ arduino/Domiotools/examples/HomeasyReceiver/HomeasyReceiver.ino
@@ -0,0 +1,59 @@
+/* Copyright (C) 2015 Sylvain Afchain
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include "homeasy.h"
+
+unsigned long last_time;
+unsigned int last_type;
+
+void setup()
+{
+  pinMode(2, INPUT);
+  Serial.begin(9600);
+
+  last_time = micros();
+  last_type = HIGH;
+}
+
+void loop()
+{
+  struct homeasy_payload payload;
+  unsigned int type, duration;
+  unsigned long now;
+  int rc = 0;
+
+  type = digitalRead(2);
+  if (type != last_type) {
+      now = micros();
+      duration = now - last_time;
+      last_time = now;
+
+      rc = homeasy_receive(2, last_type, duration, &payload);
+      if (rc == 1) {
+        Serial.println("Homeasy message received:");
+        Serial.println("  address: " + String(payload.address));
+        Serial.println("  receiver: " + String(payload.receiver));
+        Serial.println("  ctrl: " + String(payload.ctrl));
+        Serial.println("  group: " + String(payload.group));
+      }
+
+      last_type = type;
+  }
+}
